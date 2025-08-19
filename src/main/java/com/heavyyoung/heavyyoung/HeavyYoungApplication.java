d4eacb3188353ba1a18b5e65ebc881ad10b36d65
박형진 <gud5223@naver.com>
2025-08-19 13:43:21 +0900

    [Feat]: 프로젝트 생성

---
+++ src/main/java/com/heavyyoung/heavyyoung/HeavyYoungApplication.java
@@ -0,0 +1,13 @@
+package com.heavyyoung.heavyyoung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeavyYoungApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeavyYoungApplication.class, args);
+    }
+
+}
