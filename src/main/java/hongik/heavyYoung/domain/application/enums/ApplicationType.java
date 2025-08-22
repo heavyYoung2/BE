64e2dc72b624ace28cfb8a0a62f3f0dac6409074
박형진 <gud5223@naver.com>
2025-08-22 15:13:46 +0900

    [Chore]: 신청 타입 추가
    - LOCKER_MAIN과 LOCKER_ADDITIONAL을 묶은 LOCKER 타입
    - EnumSet으로 구현

--- src/main/java/hongik/heavyYoung/domain/application/enums/ApplicationType.java
+++ src/main/java/hongik/heavyYoung/domain/application/enums/ApplicationType.java
@@ -1,7 +1,11 @@
 package hongik.heavyYoung.domain.application.enums;
 
+import java.util.EnumSet;
+
 public enum ApplicationType {
     LOCKER_MAIN, // 사물함 정규 신청
     LOCKER_ADDITIONAL, // 사물함 추가 신청
-    SNACK // 간식 행사 신청
-}
+    SNACK; // 간식 행사 신청
+
+    public static final EnumSet<ApplicationType> LOCKER = EnumSet.of(LOCKER_MAIN, LOCKER_ADDITIONAL);
+    }
