42aa4b662e7b6e4a85cc2679d62f6db4659a4f38
gud0217 <gud5223@naver.com>
2025-09-01 17:23:35 +0900

    [Fix] 에러 코드 값 재정의 - 성공 시 응답과 코드가 동일하여 중복되는 문제 해결

--- src/main/java/hongik/heavyYoung/global/apiPayload/status/ErrorStatus.java
+++ src/main/java/hongik/heavyYoung/global/apiPayload/status/ErrorStatus.java
@@ -10,8 +10,7 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorStatus implements BaseErrorCode {
 
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON_001", "서버 에러, 관리자에게 문의 바랍니다."),
-
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "SERVER_001", "서버 에러, 관리자에게 문의 바랍니다."),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "USER_001", "해당 유저가 존재하지 않습니다.");
 
     private final HttpStatus httpStatus;
