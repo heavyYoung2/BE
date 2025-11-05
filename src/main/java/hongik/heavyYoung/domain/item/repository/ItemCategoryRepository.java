1f78fba334609e8aff556ce1f84802dfab453151
박형진 <gud5223@naver.com>
2025-11-05 13:43:52 +0900

    [Refactor]: 제네릭 타입 변경

--- src/main/java/hongik/heavyYoung/domain/item/repository/ItemCategoryRepository.java
+++ src/main/java/hongik/heavyYoung/domain/item/repository/ItemCategoryRepository.java
@@ -3,5 +3,5 @@ package hongik.heavyYoung.domain.item.repository;
 import hongik.heavyYoung.domain.item.entity.ItemCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ItemCategoryRepository extends JpaRepository<ItemCategory, Integer> {
+public interface ItemCategoryRepository extends JpaRepository<ItemCategory, Long> {
 }
--- src/main/java/hongik/heavyYoung/domain/item/repository/ItemRepository.java
+++ src/main/java/hongik/heavyYoung/domain/item/repository/ItemRepository.java
@@ -4,6 +4,6 @@ import hongik.heavyYoung.domain.item.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface ItemRepository extends JpaRepository<Item, Integer> {
+public interface ItemRepository extends JpaRepository<Item, Long> {
 
 }
