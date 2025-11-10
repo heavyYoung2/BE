4d001e0a0642aee38f0d74db0f34eb4e0ef34408
yhi9839 <yhi9839@gmail.com>
2025-11-10 15:59:22 +0900

    fix : AuthRestController 스웨거 정보 수정

--- src/main/java/hongik/heavyYoung/domain/member/controller/AuthRestController.java
+++ src/main/java/hongik/heavyYoung/domain/member/controller/AuthRestController.java
@@ -28,15 +28,17 @@ public class AuthRestController {
     }
     // TODO : redis 설정 이후 javaMailSender로 구현
 
-    @Operation(summary = "학교 이메일 인증번호 인증 API")
+    @Operation(summary = "학교 이메일 인증번호 인증 API",
+                description = "학교 이메일을 통해 받은 인증번호를 인증합니다.")
     @PostMapping("/verify-code")
-    public ApiResponse verifyCode(@RequestParam("code") String code){
+    public ApiResponse<?> verifyCode(@RequestParam("code") String code){
         return null;
     }
     // TODO : redis 설정 이후 javaMailSender로 구현 이후 구현
 
 
-    @Operation(summary = "회원 가입 API")
+    @Operation(summary = "회원 가입 API",
+                description = "이메일, 비밀번호, 이름, 학번, 학과, 전화번호를 통해 회원가입을 합니다.")
     @PostMapping ("/sign-in")
     public ApiResponse<AuthResponseDTO.AuthSignUpResponseDTO> signIn(
             @RequestBody @Valid AuthRequestDTO.AuthSignUpRequestDTO authRequestDTO
@@ -46,21 +48,24 @@ public class AuthRestController {
     }
 
 
-    @Operation(summary = "로그인 API")
+    @Operation(summary = "로그인 API",
+                description = "이메일, 비밀번호를 통해 로그아웃을 합니다.")
     @PostMapping ("/login")
-    public ApiResponse login(@RequestParam("email") String email, @RequestParam("password") String password){
+    public ApiResponse<?> login(@RequestParam("email") String email, @RequestParam("password") String password){
         return null;
     }
 
-    @Operation(summary = "로그아웃 API")
+    @Operation(summary = "로그아웃 API",
+                description = "로그아웃을 합니다.")
     @PostMapping ("/logout")
-    public ApiResponse logout(){
+    public ApiResponse<?> logout(){
         return null;
     }
 
-    @Operation(summary = "임시 비밀번호 발급 API")
+    @Operation(summary = "임시 비밀번호 발급 API",
+                description = "등록된 이메일을 통해 임시 비밀번호가 발급됩니다. 발급이 되는 즉시 비밀번호가 업데이트 됩니다.")
     @PostMapping("/tmp-password")
-    public  ApiResponse getTmpPassword(@RequestParam("password") String password){
+    public  ApiResponse<?> getTmpPassword(@RequestParam("password") String password){
         return null;
     }
 
